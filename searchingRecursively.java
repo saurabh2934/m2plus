51e70974e4030aa2d24d6216e03934da673b42d3
saurabh2934 <sk3620559@gmail.com>
2023-01-17 07:01:28 +0530

    checking array is sorted or not (recursivly)

---
+++ searchingRecursively.java
@@ -0,0 +1,26 @@
+public class sollution{
+	public static void isSorted(int arr[])
+	{
+	 if(arr.length == 1){ // base case
+	   return true;
+	   }
+	if(arr[o] > arr[1]){ // if first element is greater than second element it means array is not in sorted condition
+	   return false;
+	   }
+        // lets coppy rest of array in another small array
+	int smallarray=new int[arr.length -1];
+	for(int i=1;i<arr.lenght;i++){
+		smallarray[i-1]=arr[i];
+		}
+        boolean issmallarraysorted = isSorted(smallarray);
+		return issmallarraysorted;
+		}
+     public static void main (String[]agrs){
+	int arr[]={1,2,3,4,5,6,7,8};
+	boolean ans=isSorted(arr);
+        if(ans == true){
+		System.out.println("array is sorted");
+		}else
+                  {
+                   System.out.println("array is not sorted");
+                  }   
